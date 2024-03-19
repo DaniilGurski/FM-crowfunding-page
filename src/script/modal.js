ec158c1c7befb90090b5a4cbffd29a9139412fe9
DaniilGurski <daniil.gurski@elev.ga.ntig.se>
2024-03-19 22:00:55 +0100

    grid system update

---
+++ src/script/modal.js
@@ -0,0 +1,7 @@
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector("#close-modal");
+
+closeModal.addEventListener("click", () => {
+    modal.showModal();
+})
+
